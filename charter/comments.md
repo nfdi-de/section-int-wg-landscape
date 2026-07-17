d51360b81e132c694acb41773612800e5dcc1378
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-07-17 17:58:03 -0400

    Add charter (#3)

---
+++ charter/comments.md
@@ -0,0 +1,64 @@
+# Charter Notes
+
+## Comment 1 from Najla Rettberg
+
+Regarding the "Goals" introduction, Najla writes:
+
+> I think it would also be good to take the NFDI strategy as a starting point
+> and see where we need to make strategic connections at international level on
+> e,g training, operational models, metadata standards - where and if it makes
+> sense
+
+I think this is addressed by the first sentence of goal 1 - what are the
+aspirational interactions from NFDI at the top level? Task 1 concretely
+addresses assembling previous reporting materials, I will add a point about
+adding more generic NFDI strategic documentation to our "assembly of prior
+material"
+
+Training and metadata standards are immediately out of scope for this WG/section
+and are already covered by the sections/working groups about those topics.
+
+It's not clear what you mean by operational models, but I would also guess there
+should be a specific WG about something like that
+
+## Comment 2 from Ulrik Stervbo
+
+Regarding "Task 2", Ulrik writes:
+
+> Which information can be collected here that cannot be extracted in Task 1? Or
+> just a 30 minutes conversation with each consortium speaker.
+
+Would it make sense to ask about challenges and needs and include the wider
+NFDI?
+
+The idea of the survey is it can be ongoing forever and is a passive way of us
+collecting information.
+
+You're right that going out actively might also be a good idea. Like most
+things, the ability to do this will be limited by effort available by WG
+members. I added a sentence reflecting this as part of task 1.
+
+Not sure what you mean by "challenges and needs" but it might be getting out of
+scope
+
+## Comment 3 from Najla Rettberg
+
+Regarding "Task 5", Najla writes:
+
+> It might be good to differentiate: at the overall NFDI level - where this
+> group could support the directorate in identifying international relevant
+> events /presence and all the consortia-relevant events (which many are
+> regulars at anyway)
+
+Identifying events could be a good candidate for Phase 2 of this working group,
+but also is an additional level of complexity. We should save this idea as an
+issue on our GitHub repository
+
+## Comment 4 from Najla Rettberg
+
+Regarding "Task 5" publishing results as a website, Najla writes:
+
+> mapping RDA groups is a big task but a doable one, that requires ongoing
+> commitment and it's fiddly but would be worth it to ensure we have no overlaps
+> within the consortia and get more consortia members involved. Big overlap with
+> the section work too
---
+++ charter/mermaid_filter.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env -S uv run --script
+
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "pandocfilters>=1.5.1",
+# ]
+# ///
+
+# adapted from https://github.com/oviquezr/pandoc-mermaid-filter/blob/a663828ea69c9b5716e28ade897a8cae29863155/pandoc_mermaid_filter.py
+
+import os
+import sys
+import subprocess
+
+from pandocfilters import toJSONFilter, Para, Image
+from pandocfilters import get_filename4code, get_caption, get_extension
+
+# Environment variables with fallback values
+MERMAID_BIN = os.path.expanduser(os.environ.get('MERMAID_BIN', 'mmdc'))
+PUPPETEER_CFG = os.environ.get('PUPPETEER_CFG', None)
+
+
+def mermaid(key, value, format_, _):
+    if key != 'CodeBlock':
+        return None
+
+    [[ident, classes, keyvals], code] = value
+
+    if "mermaid" in classes:
+        caption, typef, keyvals = _get_caption(keyvals, format_)
+
+        # Use arguments in filename encoding to re-render when flags change
+        filename = get_filename4code("mermaid", code + str(keyvals))
+        filetype = get_extension(format_, "png", html="svg", latex="png")
+
+        src = filename + '.mmd'
+        dest = filename + '.' + filetype
+
+        if not os.path.isfile(dest):
+            txt = code.encode(sys.getfilesystemencoding())
+            with open(src, "wb") as f:
+                f.write(txt)
+
+            # Default command to execute
+            cmd = [MERMAID_BIN, "-i", src, "-o", dest]
+
+            if PUPPETEER_CFG is not None:
+                cmd.extend(["-p", PUPPETEER_CFG])
+
+            if os.path.isfile('.puppeteer.json'):
+                cmd.extend(["-p", ".puppeteer.json"])
+
+            if os.path.isfile('.mermaid-config.json'):
+                cmd.extend(["-c", ".mermaid-config.json"])
+
+            if os.path.isfile('.mermaid.css'):
+                cmd.extend(["-C", ".mermaid.css"])
+
+            for it in keyvals:
+                if it[0] == 'scale':
+                    cmd.extend(["-s", it[1]])
+
+            subprocess.check_call(cmd, stdout=sys.stderr)
+            sys.stderr.write('Created image ' + dest + '\n')
+
+        return Para([Image([ident, [], keyvals], caption, [dest, typef])])
+
+
+def _get_caption(item, fmt: str):
+    captions, typef, keyvals = get_caption(item)
+    # print('Originals: ',captions,typef,keyvals, file=sys.stderr)
+    for caption in captions:
+        if caption['c'] is None and caption['c']:
+            continue
+        caption['t'] = 'RawInline'
+        caption['c'] = [fmt, to_format(caption['c'], fmt)]
+    # print('Captions: ', captions, 'typef: ', typef, 'keyvals: ', keyvals, file=sys.stderr)
+    return captions, typef, keyvals
+
+
+def to_format(txt: str, fmt: str) -> str:
+    command = ['pandoc', '-f', 'markdown', '-t', fmt]
+    p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = p.communicate(input=txt.encode())[0].decode()
+    # print("Converted to:", output, file=sys.stderr)
+    return output
+
+
+def main():
+    toJSONFilter(mermaid)
+
+
+if __name__ == "__main__":
+    main()
